030aae4a534b736e22e0f18c4750e2977812d278
VictoriaaRoman <victoria.roman@estudiantat.upc.edu>
2023-03-21 18:29:05 +0100

    proves

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,7 +35,7 @@ export class AppComponent {
     var answer = confirm('Estas seguro de querer eliminarlo?');
     if(answer){
       this._userService.eliminarUser(id).subscribe(data => {
-        this.employees = [];
+        this.users = [];
         this.obtenerUsers();    
       }, error => {
         console.log(error);
@@ -45,7 +45,7 @@ export class AppComponent {
 
   agregarUser(){
     this._userService.añadirUser(this.model).subscribe(data => {
-      this.employees = [];
+      this.users = [];
       this.obtenerUsers();
       this.model = {_id:'',name:'',surname:'',email:'',password:''};  
     }, error => {
@@ -57,7 +57,7 @@ export class AppComponent {
     this._userService.actualizarUser(id,this.model2).subscribe(data =>{
       this.model2 = {_id:'',name:'',surname:'',email:'',password:''};
       this.hideUpdate = true;
-      this.employees = [];
+      this.users = [];
       this.obtenerUsers();
     }, error => {
       console.log(error);
@@ -66,7 +66,7 @@ export class AppComponent {
 
   title:string = 'Example APIRest CRUD';  
 
-  employees: User [] = [];
+  users: User [] = [];
 
   model:User = {_id:'',name:'',surname:'',email:'',password:''};
   model2:User = {_id:'',name:'',surname:'',email:'',password:''};
@@ -76,11 +76,11 @@ export class AppComponent {
   myValue = 0;
   editUser(i:number):void{
     this.hideUpdate = false;
-    this.model2._id = this.employees[i]._id;
-    this.model2.name = this.employees[i].name;
-    this.model2.surname = this.employees[i].surname;
-    this.model2.email = this.employees[i].email;
-    this.model2.password = this.employees[i].password;
+    this.model2._id = this.users[i]._id;
+    this.model2.name = this.users[i].name;
+    this.model2.surname = this.users[i].surname;
+    this.model2.email = this.users[i].email;
+    this.model2.password = this.users[i].password;
     this.myValue = i;
   }
   
